a20f21221496d22fd610cda74734899284075d97
Cole Vanacore <colevanacore@gmail.com>
2019-03-13 14:23:58 -0600

    Begin setting up fetch request to get all user data

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      users: []
+    }
+  },
+  created() {
+    this.fetchUserData()
+  },
+  methods: {
+    async fetchUserData () {
+      let url =  'https://frontiercodingtests.azurewebsites.net/api/accounts/getall';
+      try {
+        const response = await fetch(url)
+        const result = await response.json()
+        console.log(result)
+      } catch(err) {
+        console.log(err)
+      }
+
+    }
   }
 }
 </script>
